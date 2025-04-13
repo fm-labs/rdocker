4a1719d22a0a0727fe35e1cddb2640491ed668d4
fm-labs <flowmotionlabs@gmail.com>
2025-04-14 01:53:42 +0200

    Added default env vars

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,4 +3,9 @@ set -xe
 
 echo "Entrypoint script for RDOCKER"
 
+export RDOCKER_CONTEXT=${RDOCKER_CONTEXT:-default}
+export RDOCKER_LOCAL_SOCKET=${RDOCKER_LOCAL_SOCKET:-/rdocker/run/rdocker.sock}
+export RDOCKER_TCP_ENABLE=${RDOCKER_TCP_ENABLE:-1}
+export RDOCKER_TCP_PORT=${RDOCKER_TCP_PORT:-12345}
+
 exec "$@"
