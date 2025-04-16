492d6259fdbb0172dc9bb5a285d3b81fc97a3026
fm-labs <flowmotionlabs@gmail.com>
2025-04-16 17:39:39 +0200

    Added "verbose debug mode" (RDOCKER_DEBUG=2). Streamlined env configuration.

--- lib/env.inc.sh
+++ lib/env.inc.sh
@@ -59,3 +59,56 @@ if [ -z "$RDOCKER_REMOTE_USER" ]; then
   echoerr "RDOCKER_REMOTE_USER not defined. Exiting"
   exit 1
 fi
+
+###############################################################
+
+# INTERNAL VARIABLES
+# set the log prefix to the context
+RDOCKER_LOGPREFIX="[${RDOCKER_CONTEXT}] "
+# the docker socket on the remote host
+RDOCKER_REMOTE_SOCKET=${RDOCKER_REMOTE_SOCKET:-/var/run/docker.sock}
+# the tmp directory on the local host
+RDOCKER_LOCAL_TMPDIR=${RDOCKER_LOCAL_TMPDIR:-/tmp}
+# the docker socket on the local host
+RDOCKER_LOCAL_SOCKET=${RDOCKER_LOCAL_SOCKET:-}
+if [ -z "$RDOCKER_LOCAL_SOCKET" ]; then
+  RDOCKER_LOCAL_SOCKET=${RDOCKER_LOCAL_TMPDIR}/rdocker-docker.${RDOCKER_CONTEXT}.sock
+fi
+# the (R)DOCKER_HOST value used to connect to the remote host via the tunnel
+RDOCKER_HOST=unix://${RDOCKER_LOCAL_SOCKET}
+# the (Auto)SSH tunnel PID file
+RDOCKER_TUNNEL_PID_FILE=${RDOCKER_LOCAL_TMPDIR}/rdocker-ssh-tunnel.${RDOCKER_CONTEXT}.pid
+RDOCKER_SOCAT_PID_FILE=${RDOCKER_LOCAL_TMPDIR}/rdocker-socat.${RDOCKER_CONTEXT}.pid
+
+
+# SSH
+RDOCKER_REMOTE_SSH_KEY=${RDOCKER_REMOTE_SSH_KEY:-}
+RDOCKER_REMOTE_SSH_KEY_PASS=${RDOCKER_REMOTE_SSH_KEY_PASS:-}
+RDOCKER_REMOTE_SSH_KEY_PASS_FILE=${RDOCKER_REMOTE_SSH_KEY_PASS_FILE:-}
+if [ -z "$RDOCKER_REMOTE_SSH_KEY" ]; then
+  if [ -f "${RDOCKER_HOME}/${RDOCKER_CONTEXT}.key" ]; then
+    RDOCKER_REMOTE_SSH_KEY="${RDOCKER_HOME}/${RDOCKER_CONTEXT}.key"
+
+    # check corresponding passphrase file
+    if [ -f "${RDOCKER_HOME}/${RDOCKER_CONTEXT}.key.pass" ]; then
+      RDOCKER_REMOTE_SSH_KEY_PASS_FILE="${RDOCKER_HOME}/${RDOCKER_CONTEXT}.key.pass"
+    fi
+  fi
+fi
+
+# read the passphrase from the file, if set
+if [ -n "$RDOCKER_REMOTE_SSH_KEY_PASS_FILE" ]; then
+  if [ -f "$RDOCKER_REMOTE_SSH_KEY_PASS_FILE" ]; then
+    RDOCKER_REMOTE_SSH_KEY_PASS=$(cat $RDOCKER_REMOTE_SSH_KEY_PASS_FILE)
+  else
+    echoerr "SSH key passphrase file not found: $RDOCKER_REMOTE_SSH_KEY_PASS_FILE"
+    exit 1
+  fi
+fi
+echolog "RDOCKER_REMOTE_SSH_KEY: $RDOCKER_REMOTE_SSH_KEY"
+echolog "RDOCKER_REMOTE_SSH_KEY_PASS_FILE: $RDOCKER_REMOTE_SSH_KEY_PASS_FILE"
+#echolog "RDOCKER_REMOTE_SSH_KEY_PASS: $RDOCKER_REMOTE_SSH_KEY_PASS"
+
+# TCP Proxy
+RDOCKER_TCP_ENABLE=${RDOCKER_TCP_ENABLE:-0}
+RDOCKER_TCP_PORT=${RDOCKER_TCP_PORT:-12345}
