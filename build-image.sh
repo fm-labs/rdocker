de6cb31c50c1d673ca078a203bfec81e41c032de
fm-labs <flowmotionlabs@gmail.com>
2025-05-30 15:30:45 +0200

    Added SBOM

--- build-image.sh
+++ build-image.sh
@@ -4,6 +4,7 @@ TAGNAME=rdocker
 
 echo "Building multi-platform image..."
 docker buildx build -t $TAGNAME \
+  --sbom=true --provenance=true \
   --progress=plain \
   --platform linux/amd64,linux/arm64 \
   -f ./Dockerfile \
@@ -18,3 +19,11 @@ rm -rf build/artifacts/
 mkdir -p build/artifacts/
 docker scout cves --format packages $TAGNAME > build/artifacts/$TAGNAME-scout.txt
 docker scout recommendations -o build/artifacts/$TAGNAME-scout-recommendations.txt
+docker scout sbom $TAGNAME > build/artifacts/$TAGNAME-sbom.json
+
+syft $TAGNAME -o spdx-json > build/artifacts/$TAGNAME-syft-sbom-spdx.json
+
+#oras attach docker.io/fmlabs/rdocker:latest \
+#  --artifact-type application/spdx+json \
+#  build/artifacts/$TAGNAME-syft-sbom-spdx.json \
+#  sbom.spdx.json:application/spdx+json
